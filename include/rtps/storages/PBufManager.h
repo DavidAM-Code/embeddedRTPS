4a9745fb64bd0156ee2f11f493ddc6dee33e64c3
david-murach <david.murach@rwth-aachen.de>
2025-06-27 19:34:24 +0200

    pbufmanager

---
+++ include/rtps/storages/PBufManager.h
@@ -0,0 +1,25 @@
+//
+// Created by david-murach on 6/21/25.
+//
+
+#ifndef PBUFMANAGER_H
+#define PBUFMANAGER_H
+
+#include "rtps/storages/PBufWrapper.h"
+#include "rtps/common/types.h"
+
+namespace rtps {
+    class PBufManager {
+    public:
+        virtual ~PBufManager() = default;
+
+        virtual rtps::PBufWrapper* getData(Guid_t) = 0;
+
+        virtual bool isInRetransmitMode() const = 0;
+
+        virtual Guid_t getRetransmitGuid() const = 0;
+    };
+
+} // namespace rtps
+
+#endif //PBUFMANAGER_H
